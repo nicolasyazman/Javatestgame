4be7af9f414fe3a2700a40889481ff2840ecf9f3
nicol <nicol@host.docker.internal>
2024-07-20 21:48:40 +0200

    Added chat history, changed tests to use annotations and using @BeforeClass tag to set up the network once for multiple tests.

--- core/Lobby.java
+++ core/Lobby.java
@@ -10,31 +10,48 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Lobby {
 
 	public static int CHATROOM_PORT = 8888;
 	public static int MAX_MESSAGE_SIZE_BYTES = 1000;
+	public static int MAX_CHAT_HISTORY_SIZE = 100;
 	
 	private Player host;
 	private ArrayDeque<Player> playersInLobby;
-	private DatagramSocket senderSocket;
+	private DatagramSocket senderSocket = null;
+	private ArrayDeque<String> chatHistory;
+	
 	
 	// Mostly for testing for now.
 	private byte[] receivedPacketEcho;
+	
+	// For testing
+	public ArrayDeque<String> getChatHistory() {
+		return chatHistory;
+	}
 	
 	public Lobby(Player host) {
 		this.host = host;
 		this.playersInLobby = new ArrayDeque<Player>();
 		this.receivedPacketEcho = new byte[MAX_MESSAGE_SIZE_BYTES];
+		this.chatHistory = new ArrayDeque<String>();
 		Arrays.fill(this.receivedPacketEcho, (byte)0);
 		this.tryToConnect();
+		ReceiverThread receiverThread = new ReceiverThread();
+		receiverThread.start();
+		
 	}
 	
 	public void tryToConnect() {
 		try {
-			this.senderSocket = new DatagramSocket(null);
-			this.senderSocket.bind(new InetSocketAddress(host.getIPAddress(), CHATROOM_PORT));
+			if (this.senderSocket == null) {
+				this.senderSocket = new DatagramSocket(null );
+				this.senderSocket.bind(new InetSocketAddress(host.getIPAddress(), CHATROOM_PORT));
+		
+			}
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -52,9 +69,17 @@ public class Lobby {
 			while (true) {
 				DatagramPacket packetReceive = new DatagramPacket(receivedPacketEcho, MAX_MESSAGE_SIZE_BYTES);
 				try {
+					
 					senderSocket.receive(packetReceive);
 					System.out.println("Received packet: " + new String(packetReceive.getData()));
-					break;
+					// Adding it to chat history
+					while (chatHistory.size() >= MAX_CHAT_HISTORY_SIZE) {
+						chatHistory.pop(); // We don't want to keep an archive of the chat so just depop.
+					}
+					
+					chatHistory.add(getReceivedPacketEcho());
+					Arrays.fill(receivedPacketEcho, (byte)0);
+					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -65,15 +90,14 @@ public class Lobby {
 	}
 	
 	public void sendMessageFromHostBroadcast(String message) {
-		 DatagramSocket s;
+		
 		try {
 			// First we create a datagram socket to send datagram packets.
 			// Why Datagram? UDP because we don't care about the proper reception of the packets or not.
 					
-			byte[] receiveBuffer = new byte[message.getBytes().length];
+			//byte[] receiveBuffer = new byte[message.getBytes().length];
 	
-			ReceiverThread receiverThread = new ReceiverThread();
-			receiverThread.start();
+			
 			
 			//s.bind(new InetSocketAddress(host.getIPAddress(), CHATROOM_PORT));
 			
@@ -112,5 +136,29 @@ public class Lobby {
 		}
 		
 		return new String(receivedPacketEcho, 0, idx, StandardCharsets.UTF_8);
+	}
+	
+	public String getChatHistoryMessage(int messageNumber) {
+		if (messageNumber < 0) {
+			return null;
+		}
+		if (messageNumber >= chatHistory.size()) {
+			return null;
+		}
+		
+		Iterator<String> i = chatHistory.iterator();
+		int idx = 0;
+		
+		String currentMessage;
+		while (i.hasNext())
+		{
+			currentMessage = i.next();
+			if (messageNumber == idx) {
+				return currentMessage;
+			}
+			idx++;
+		}
+		
+		return null;
 	}
 }
--- tests/LobbyTest.java
+++ tests/LobbyTest.java
@@ -1,25 +1,40 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import core.Lobby;
 import core.Network;
 import junit.framework.TestCase;
 
-public class LobbyTest extends TestCase {
+public class LobbyTest  {
 
+	private static Lobby lobby;
+	
+	@BeforeClass
+	public static void setup() {
+		
+			Network network = new Network();
+			String myIp;
+			lobby = network.createNewLobbyOrReturnsExistingLobby();
+			myIp = Network.getIpAddress();
+			network.joinLobby(myIp);
+	}
 	
 	/**
 	 * Testing the beginning of a chatroom.
 	 */
+	@Test
 	public void testSendingMessageBroadcast() {
 		// Arrange
-		Network network = new Network();
-		String myIp;
+		
 		String messageToSend = "testSendingMessageBroadcast";
 		
 		// Act
-		Lobby lobby = network.createNewLobbyOrReturnsExistingLobby();
-		myIp = Network.getIpAddress();
-		network.joinLobby(myIp);
 		lobby.sendMessageFromHostBroadcast(messageToSend);
 		
 		try {
@@ -30,8 +45,59 @@ public class LobbyTest extends TestCase {
 		}
 		
 		// Assert
-		System.out.println("Received message: " + lobby.getReceivedPacketEcho());
-		System.out.println(lobby.getReceivedPacketEcho().length());
-		assertEquals(messageToSend, lobby.getReceivedPacketEcho());
+		assertEquals(messageToSend, lobby.getChatHistory().getLast());
+	}
+	
+	@Test
+	public void testSendingMultipleMessagesBroadcast() {
+		// Arrange
+		String[] messagesToSend = {"testSendingMessageBroadcast_message1", "testSendingMessageBroadcast_mess2"}; 
+		String[] messageReceived = new String[messagesToSend.length];
+		
+		// Act
+		for (int i = 0; i < messagesToSend.length; i++) {
+			System.out.println(messagesToSend[i]);
+			lobby.sendMessageFromHostBroadcast(messagesToSend[i]);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			messageReceived[i] = lobby.getChatHistory().getLast();
+		}
+		
+		// Assert
+		assertEquals(messagesToSend[0], messageReceived[0]);
+		assertEquals(messagesToSend[1], messageReceived[1]);
+	}
+	
+	@Test
+	public void testSendingMessagesOverTheLimit() {
+	
+			// Arrange
+			String messageToSend = "testSendingMessageBroadcast_message";
+			int numberOfMessagesToSend = Lobby.MAX_CHAT_HISTORY_SIZE*2;
+			String[] messageReceived = new String[numberOfMessagesToSend];
+			
+			// Act
+			for (int i = 0; i < numberOfMessagesToSend; i++) {
+				
+				lobby.sendMessageFromHostBroadcast(messageToSend+i);
+				
+				try {
+					Thread.sleep(30);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				messageReceived[i] = lobby.getChatHistory().getLast();
+			}
+			
+			// Assert
+			for (int i = 0; i < numberOfMessagesToSend; i++) {
+				assertEquals(messageToSend+i, messageReceived[i]);
+			}
 	}
 }
